074399b975d12d2d4a9927d5ccc7592ac971ae4a
gabriel <jesusdesilva78@gmail.com>
2022-12-16 12:43:31 -0300

    Export Stickers

---
+++ stickers.js
@@ -0,0 +1,54 @@
+
+const Stickers =
+ [
+    {
+        sticker: 'imgs_Stickers/ds_emote-0.png',
+        cor: "#A60505",
+        id: "Gael"
+    },
+    {
+
+        sticker: 'imgs_Stickers/ds_emote-1.png',
+        cor: 'gray',
+        id: "Lothric Knight"
+    },
+
+    {
+        sticker: 'imgs_Stickers/ds_emote-2.png',
+        cor: 'aqua',
+        id: 'gundyr'
+    },
+    {
+        sticker: 'imgs_Stickers/ds_emote-3.webp',
+        cor: 'orange',
+        id: 'Elite Knight'
+
+    },
+    {
+        sticker: 'imgs_Stickers/ds_emote-4.webp',
+        cor: 'red',
+        id: 'Ornstein'
+    },
+    {
+        sticker: 'imgs_Stickers/ds_emote-5.png',
+        cor: '#E2B501',
+        id: 'Nameless king'
+
+    },
+    {
+        sticker: 'imgs_Stickers/ds_emote-6.png',
+        cor: '#2D2440',
+        id: 'Nito'
+
+    },
+    {  
+        //objeto vazio;
+       //esse objeto fica vazio para que o objeto 6 apareça na tela;
+    }
+];
+export default Stickers
+
+
+
+
+

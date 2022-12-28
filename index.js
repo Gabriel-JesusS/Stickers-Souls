b2da6e6fa9a8ffba92f73f5abe850967b51576ee
gabriel <jesusdesilva78@gmail.com>
2022-12-27 23:04:56 -0300

    Index Codes

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 import Stickers from './stickers.js';
 
 const stickers = document.querySelector('#screen');
-const range = document.querySelector('#range');
 const BackgroundImg = document.querySelector('img');
 const id = document.querySelector('#names');
 const BtAuto = document.querySelector('#AutoBt');
@@ -14,27 +13,15 @@ const BtRight = document.querySelector('#right');
 //variaveis globais
 let ClickDirection = 0;
 let index_Stickers = 0;
-range.value = 0;
 id.textContent = Stickers[index_Stickers].id;
 stickers.src = Stickers[index_Stickers].sticker;
 BackgroundImg.style.background = Stickers[index_Stickers].cor;
 
-//range dos stickers 
-range.addEventListener('input', function () {
-  for (let index_Stickers = 0; index_Stickers < range.value; index_Stickers++) {
-    id.textContent = Stickers[index_Stickers].id
-    stickers.src = Stickers[index_Stickers].sticker;
-    BackgroundImg.style.background = Stickers[index_Stickers].cor;
-  }
-});
-
 var clicks = null; // contador de clicks
 BtAuto.addEventListener("click", () => {
   clicks++;
-  if (clicks == 1) {
-    //muda a quantidade maxima do rage pra modo Auto
-    range.max = 6;
 
+  if (clicks == 1) {
     //ativação da luz superior direita
     light.style.background = "#00FF00";
     light.style.boxShadow = '-1px 0px 15px green';
@@ -50,7 +37,6 @@ BtAuto.addEventListener("click", () => {
     const stop = setInterval(() => {
       id.textContent = Stickers[index_Stickers].id;
       BackgroundImg.style.background = Stickers[index_Stickers].cor;
-      range.value = index_Stickers;
       stickers.src = Stickers[index_Stickers].sticker;
       index_Stickers++;
 
@@ -58,14 +44,12 @@ BtAuto.addEventListener("click", () => {
       if (index_Stickers == 8) {
         clearInterval(stop)
         index_Stickers = 0;
-        range.value = 0;
         BackgroundImg.style.background = Stickers[index_Stickers].cor;
         id.textContent = Stickers[index_Stickers].id;
         stickers.src = Stickers[index_Stickers].sticker;
         light.style.background = null;
         light.style.boxShadow = null;
         clicks = 0;
-        range.max = 7;
       }
     }, 900);
   }
@@ -75,7 +59,6 @@ BtAuto.addEventListener("click", () => {
 BtRight.addEventListener('click', () => {
   ClickDirection++;
   ClickDirection == 7 ? ClickDirection = 0 :'';
-  range.value = ClickDirection;
   BackgroundImg.style.background = Stickers[ClickDirection].cor;
   id.textContent = Stickers[ClickDirection].id;
   stickers.src = Stickers[ClickDirection].sticker;
@@ -84,7 +67,6 @@ BtRight.addEventListener('click', () => {
 BtLeft.addEventListener('click', () => {
   ClickDirection--;
   ClickDirection <= 0 ? ClickDirection = 0 :'';
-  range.value = ClickDirection;
   BackgroundImg.style.background = Stickers[ClickDirection].cor;
   id.textContent = Stickers[ClickDirection].id;
   stickers.src = Stickers[ClickDirection].sticker;
